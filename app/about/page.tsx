ec16af90cfc3fcaf1b43ab643695a73bdf76f3aa
LIU XIANG <qianiaoo@gmail.com>
2023-05-10 22:52:54 +0900

    [update]footer

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,15 +11,13 @@ export default function About() {
       </PageTitle>
       <div className="grid grid-cols-1 sm:grid-cols-2 w-100 ltr justify-center flex-wrap">
         <div className="flex flex-col justify-evenly align-middle text-xl">
+          <p className="py-2">中国出身で、今は広島市立大学の4年生。</p>
           <p className="py-2">
-            中国河南省出身で、今は広島市立大学に留学しています。
-          </p>
-          <p className="py-2">
-            知能工学科データ工学研究室に所属しています。最近はtext-to-imageに興味があります。
+            知能工学科データ工学研究室に所属。最近はtext-to-imageに興味がある。
           </p>
           <p className="py-2">
             <span className="font-semibold">趣味:</span>{" "}
-            山登るのが好きです。カメラも好きです。旅行も好きです。漫画も好きです。絵は友達が書いてくれました。
+            山登り、カメラ、アニメ、旅行
           </p>
         </div>
         <div className="relative sm:order-none order-first w-100 sm:w-72 h-96 overflow-hidden rounded-lg m-5 hover:scale-[1.02] transition-all hover:shadow-xl">
